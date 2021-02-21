c21f336ce18932f2b9ef689bc61b24435f6405b2
kizwond <kizwond@gmail.com>
2021-02-21 12:47:32 +0900

    package install and navbar container start

---
+++ src/components/navbar/NavbarContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class NavbarContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <div className="navbar_container">
+                hello navbar!!!!
+            </div>
+          );
+    }
+}
+ 
+export default NavbarContainer;
